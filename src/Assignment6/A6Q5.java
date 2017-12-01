ddd05d27f51385516e5ba670a29b320a641cf42b
goodm9679 <goodm9679@HRH-IC0027342.wrdsb.ca>
2017-12-01 13:36:20 -0500

    assignment 6 complete

--- src/Assignment6/A6Q5.java
+++ src/Assignment6/A6Q5.java
@@ -21,7 +21,7 @@ public class A6Q5 {
         int total = input.nextInt();
         if (total > 1) {
             System.out.println("Enter the " + total + " marks:");
-            //array with two parts
+            //array with chosen number of parts
             double[] array = new double[total];
             for (int i = 0; i < total; i++) {
                 array[i] = input.nextDouble();
@@ -42,16 +42,23 @@ public class A6Q5 {
             System.out.print("arranged from lowest to highest, the order is ");
             for (int i = 0; i < array.length; i++) {
                 System.out.print(array[i]);
+                //adds a comma after every number but the last
                 if (i != array.length - 1) {
                     System.out.print(", ");
                 }
             }
+            //ends first statement
             System.out.println(".");
+            //calculates median for odd number of inputs
             int med = total / 2;
+            //checks if the number of inputs is even
             if (total % 2 == 0) {
+                //calculates average of two medians
                 double ian = (array[med] + array[med-1])/2;
+                //outputs even median
                 System.out.println("The median is " + ian + ".");
             }else{
+                //outputs odd median
                 System.out.println("The median is " + array[med] + ".");
             }
         } else {
