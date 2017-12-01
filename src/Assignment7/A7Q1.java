c81114331a199cc4c5c08ed537dab3847ac601aa
goodm9679 <goodm9679@HRH-IC0027342.wrdsb.ca>
2017-12-01 13:40:32 -0500

    assignment 6 complete

--- src/Assignment7/A7Q1.java
+++ src/Assignment7/A7Q1.java
@@ -4,50 +4,16 @@
  */
 package Assignment7;
 
-import java.util.Scanner;
-
 /**
  *
  * @author goodm9679
  */
 public class A7Q1 {
 
-    //method for calculating grade
-    public void grader(double mark) {
-        String grade = "";
-        //coin sorter. value slids down track until it falls into a hole it fits into
-        if (mark < 50) {
-            grade = "F";
-        } else if (mark >= 50 && mark < 60) {
-            grade = "D";
-        } else if (mark >= 60 && mark < 70) {
-            grade = "C";
-        } else if (mark >= 70 && mark < 80) {
-            grade = "B";
-        } else if (mark >= 80) {
-            grade = "A";
-        }
-        System.out.println(grade);
-    }
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        A7Q1 run = new A7Q1();
-        //infinite loop
-        while (true) {
-            System.out.println("Enter a mark. type \"Quit\" to quit");
-            String text = input.nextLine();
-            if (text.equalsIgnoreCase("Quit")) {
-                break;
-            } else {
-                //changes input text to a double
-                double mark = Double.parseDouble(text);
-                //run method
-                run.grader(mark);
-            }
-        }
+        // TODO code application logic here
     }
 }
